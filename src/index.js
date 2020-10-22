0514a826a9f26326148c59c48d5506028014f3f4
caioalexandrebr <contato@caioalexandre.com>
2020-10-22 17:09:20 -0300

    feat(Header): add navbar config and assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+
 import Routes from './routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import {Image} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
+import logo from './assets/instagram.png';
 import Feed from './pages/Feed';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Feed,
-  }),
+  createStackNavigator(
+    {
+      Feed,
+    },
+    {
+      headerTitleAlign: 'center',
+      defaultNavigationOptions: {
+        headerTitle: () => <Image source={logo} />,
+        headerStyle: {
+          backgroundColor: '#f5f5f5',
+        },
+      },
+    },
+  ),
 );
 
 export default Routes;
