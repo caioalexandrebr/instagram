911322aa669c9a084e321374a8f8ce11c7a87070
caioalexandrebr <contato@caioalexandre.com>
2020-10-22 17:58:31 -0300

    feat(Feed): add request for feed component style posts

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
+
+import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 const Feed = () => {
-  return <View />;
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    async function loadFeed() {
+      const response = await fetch(
+        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
+      );
+
+      const data = await response.json();
+
+      setFeed(data);
+    }
+
+    loadFeed();
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={(post) => String(post.id)}
+        renderItem={({item}) => (
+          <Post>
+            <Header>
+              <Avatar source={{uri: item.author.avatar}} />
+              <Name>{item.author.name}</Name>
+            </Header>
+            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
 };
 
 export default Feed;
---
+++ src/pages/Feed/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+export const Post = styled.View`
+  margin-top: 10px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  flex-direction: row;
+  padding: 15px;
+`;
+
+export const Avatar = styled.Image`
+  border-radius: 16px;
+  height: 32px;
+  margin-right: 10px;
+  width: 32px;
+`;
+
+export const Name = styled.Text`
+  color: #333;
+  font-weight: bold;
+`;
+
+export const PostImage = styled.Image`
+  aspect-ratio: ${(props) => props.ratio};
+  width: 100%;
+`;
+
+export const Description = styled.Text`
+  line-height: 18px;
+  padding: 15px;
+`;
