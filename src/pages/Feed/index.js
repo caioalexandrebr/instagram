3f2e90d312a39ffb0ff481307f733056731b7d35
caioalexandrebr <contato@caioalexandre.com>
2020-10-23 11:52:44 -0300

    feat(Feed): add loading feed posts

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,19 +5,28 @@ import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 const Feed = () => {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  async function loadPage(pageNumber = page) {
+    const limitItems = 5;
+
+    if (total && pageNumber > total) return;
+    console.log(pageNumber);
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=${limitItems}&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('x-Total-Count');
+
+    setTotal(Math.floor(totalItems / limitItems));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -25,6 +34,8 @@ const Feed = () => {
       <FlatList
         data={feed}
         keyExtractor={(post) => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
         renderItem={({item}) => (
           <Post>
             <Header>
