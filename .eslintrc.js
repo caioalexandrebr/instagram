ddf4891a310087bf0538e4fb6dcf7d5f0a73c36a
caioalexandrebr <contato@caioalexandre.com>
2020-10-24 02:20:00 -0300

    feat(Header): add clickable icons in the header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'react-native/no-inline-styles': 'off',
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {TouchableOpacity, View, Image} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-import logo from './assets/instagram.png';
+import camera from './assets/camera.png';
 import Feed from './pages/Feed';
+import logo from './assets/instagram.png';
+import send from './assets/send.png';
+import igtv from './assets/igtv.png';
 
 const Routes = createAppContainer(
   createStackNavigator(
@@ -15,6 +18,21 @@ const Routes = createAppContainer(
       headerTitleAlign: 'center',
       defaultNavigationOptions: {
         headerTitle: () => <Image source={logo} />,
+        headerLeft: () => (
+          <TouchableOpacity style={{marginLeft: 15}}>
+            <Image source={camera} />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity style={{marginRight: 15}}>
+              <Image source={igtv} />
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginRight: 15}}>
+              <Image source={send} />
+            </TouchableOpacity>
+          </View>
+        ),
         headerStyle: {
           backgroundColor: '#f5f5f5',
         },
